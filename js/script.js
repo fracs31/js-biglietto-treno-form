8de8d450ef01fa350748659bbf873b48f41871e8
fracs31 <pist.fracs95@gmail.com>
2023-02-01 16:17:47 +0100

    ADD input of the information

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 let form = document.getElementById("form"); //form
+let submit = document.getElementById("submit"); //bottone per l'invio del form
+let fullName; //nome completo
+let km; //chilometri da percorrere
+let discount; //sconto
 
-console.log(form); //stampa del tag form
+
+//Creazione del biglietto
+submit.addEventListener("click", function(){
+    //Input
+    fullName = document.getElementById("fullName").value; //prendo il nome completo
+    km = document.getElementById("km").value; //prendo i chilometri da percorrere
+    discount = document.getElementById("discount").value; //prendo lo sconto da applicare
+
+    
+
+});
